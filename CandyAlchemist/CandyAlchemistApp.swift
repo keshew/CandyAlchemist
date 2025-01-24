d520e0a2d33d6afa82e81d48001d89b641a47407
keshew <artyom.korotckov2017@yandex.ru>
2025-01-24 16:29:20 +0500

    Initial Commit

---
+++ CandyAlchemist/CandyAlchemistApp.swift
@@ -0,0 +1,17 @@
+//
+//  CandyAlchemistApp.swift
+//  CandyAlchemist
+//
+//  Created by Артём Коротков on 24.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct CandyAlchemistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
